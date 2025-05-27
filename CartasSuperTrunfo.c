5a88232fff7f26f9006e4c73d8c132ba277246b2
AleffyAndrade <aleffy_almeida@hotmail.com>
2025-05-27 09:04:43 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -20,3 +20,66 @@ int main() {
 
     return 0;
 }
+
+#include <stdio.h>
+
+int main() {
+    char estado1, estado2;
+    char codigo1[100], codigo2[100];
+    char cidade1[100], cidade2[100];
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int ponto1, ponto2;
+
+    printf("Carta 01! Complete o que se pede :-)\n");
+    printf("Digite o estado (apenas a letra inicial):\n");
+    scanf(" %c", &estado1);
+
+    printf("Digite o código do estado (letra inicial e o número de 0 a 4):\n");
+    scanf("%s", codigo1);
+
+    printf("Digite o nome completo da cidade:\n");
+    scanf("%s", cidade1);
+
+    printf("Digite a população:\n");
+    scanf("%d", &populacao1);
+
+    printf("Digite a área:\n");
+    scanf("%f", &area1);
+
+    printf("Digite o PIB:\n");
+    scanf("%f", &pib1);
+
+    printf("Digite a quantidade de pontos turísticos:\n");
+    scanf("%d", &ponto1);
+
+    printf("\nCarta 02! Complete o que se pede :-)\n");
+    printf("Digite o estado (apenas a letra inicial):\n");
+    scanf(" %c", &estado2);
+
+    printf("Digite o código do estado (letra inicial e o número de 0 a 4):\n");
+    scanf("%s", codigo2);
+
+    printf("Digite o nome completo da cidade:\n");
+    scanf("%s", cidade2);
+
+    printf("Digite a população:\n");
+    scanf("%d", &populacao2);
+
+    printf("Digite a área:\n");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB:\n");
+    scanf("%f", &pib2);
+
+    printf("Digite a quantidade de pontos turísticos:\n");
+    scanf("%d", &ponto2);
+
+    printf("\nA carta 01 é: %c, %s, %s, %d, %.2f, %.2f, %d\n",
+           estado1, codigo1, cidade1, populacao1, area1, pib1, ponto1);
+    printf("A carta 02 é: %c, %s, %s, %d, %.2f, %.2f, %d\n",
+           estado2, codigo2, cidade2, populacao2, area2, pib2, ponto2);
+
+    return 0;
+}
